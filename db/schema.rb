8a1dc058c779d454ff32a6f4e79026785d7b2ed3
vinibispo <vini.bispo015@gmail.com>
2021-09-29 21:57:01 -0300

    feat: update database with migration of post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_003949) do
+ActiveRecord::Schema.define(version: 2021_09_30_005213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2021_09_30_003949) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+  end
+
+  add_foreign_key "posts", "categories"
 end
